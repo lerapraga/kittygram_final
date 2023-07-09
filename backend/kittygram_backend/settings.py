439241e7937e93f13d41512fb8d1aff5b65b5434
Lera Praga <abastrenie@mail.ru>
2023-07-09 18:09:35 +0500

    fix after rw1_v2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,17 +2,15 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
 
-load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-123')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['kitty1.ddns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split()
 
 
 INSTALLED_APPS = [
